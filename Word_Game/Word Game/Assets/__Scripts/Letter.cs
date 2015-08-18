15856bbefb925ca82a3361d8b264c52333c09255
Zach Schwoerer <ZachS@stu.aii.edu>
2015-08-17 23:28:00 -0400

    changes

---
+++ Word_Game/Word Game/Assets/__Scripts/Letter.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Letter : MonoBehaviour {
+
+	private char _c;
+	public TextMesh tMesh;
+	public Renderer tRend;
+
+	public bool    big = false;
+
+		void Awake(){
+		tMesh = GetComponentInChildren<TextMesh>();
+		tRend = tMesh.GetComponent<Renderer> ();
+		visible = false;
+	
+	}
+
+	public char c {
+		get {
+			return(_c);
+		}
+		set {
+			_c = value;
+			tMesh.text = _c.ToString ();
+		}
+	}
+	public string str {
+		get {
+			return(_c.ToString ());
+		}
+		set {
+			c = value [0];
+		}
+	}
+	public bool visible {
+		get {
+			return(tRend.enabled);
+		}
+		set {
+			tRend.enabled = value;
+		}
+	}
+	public Color color {
+		get{
+			return(this.GetComponent<Renderer> ().material.color);
+		}
+		set {
+			this.GetComponent<Renderer> ().material.color = value;
+		}
+	}
+	public Vector3 pos {
+		set{
+			this.transform.position = value;
+		}
+	}
+}
---
+++ Word_Game/Word Game/Assets/__Scripts/WordList.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WordList : MonoBehaviour {
+
+	public static WordList S;		//singleton
+	public TextAsset wordListText;	//the text file
+	public int numToParseBeforeYield = 10000;	//parse 10000 lines at a time
+	public int wordLengthMin = 3;	// min word length
+	public int wordLengthMax = 7;	//max word length
+	public int currLine = 0;		//the index of the current line
+	public int totalLines;			//total lines in file
+	public int longWordCount;		//count for # of max lenght words
+	public int wordCount;			//the word count
+	// PRIVATE VARIABLES
+	private string[] lines;			//holds the lines as they are read in
+	private List<string> longWords;	//a list for the long (max length) words
+	private List<string> words;		//a list for the other words
+
+
+	void Awake()
+	{
+		S = this;
+	}
+
+
+
+	public void Init () 
+	{
+		lines = wordListText.text.Split ('\n');
+		totalLines = lines.Length;
+
+		StartCoroutine (ParseLines ());
+	}
+
+	public IEnumerator ParseLines(){
+		string word;
+
+		longWords = new List<string> ();
+		words = new List<string> ();
+
+		for (currLine = 0; currLine < totalLines; currLine++) {
+			word = lines[currLine];
+			if (word.Length == wordLengthMax){
+				longWords.Add(word);
+			}
+			if (word.Length >= wordLengthMin && word.Length <= wordLengthMax){
+				words.Add(word);
+			}
+
+			if (currLine % numToParseBeforeYield == 0){
+
+				longWordCount = longWords.Count;
+				wordCount = words.Count;
+
+				yield return null;
+			}
+		}
+		gameObject.SendMessage("WordListParseComplete");
+
+	}
+	public List<string> GetWords(){
+		return (words);
+	}
+	public string GetWord(int ndx){
+		return(words[ndx]);
+	}
+	public List<string> GetLongWords(){
+		return(longWords);
+	}
+	public string GetLongWord(int ndx){
+		return(longWords [ndx]);
+	}
+
+
+}
